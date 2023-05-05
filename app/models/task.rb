b5400a7e38c826355bbf3bcde8baba00c75b064b
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-05-05 19:10:04 +0900

    ステータス、優先度入力欄作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,8 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
   validates :content, presence: true
+  validates :status, presence: true
+  validates :priority, presence: true
 
   scope :sort_expired_at, -> {order(expired_at: :desc)}
 end
