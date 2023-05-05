be09b0d659efb0e383a2c64349e4ae315b537b9c
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-05-05 17:58:41 +0900

    ステータス、優先度カラムを追加

---
+++ db/migrate/20230505085442_add_columns_to_tasks.rb
@@ -0,0 +1,6 @@
+class AddColumnsToTasks < ActiveRecord::Migration[6.1]
+  def change
+    add_column :tasks, :status, :integer
+    add_column :tasks, :priority, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_05_000559) do
+ActiveRecord::Schema.define(version: 2023_05_05_085442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2023_05_05_000559) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "expired_at", default: -> { "now()" }, null: false
+    t.integer "status"
+    t.integer "priority"
   end
 
 end
