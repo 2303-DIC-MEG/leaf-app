85e6d67c728e0e35cce2a126be4377c496a15ba7
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-05-06 13:09:44 +0900

    ステータスカラムのデータ型変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_05_085442) do
+ActiveRecord::Schema.define(version: 2023_05_06_040525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2023_05_05_085442) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "expired_at", default: -> { "now()" }, null: false
-    t.integer "status"
+    t.string "status"
     t.integer "priority"
   end
 
