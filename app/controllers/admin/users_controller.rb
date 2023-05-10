f0dfc4ee124487d7e84ccc76822f5f625d6a342c
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-05-10 19:44:45 +0900

    管理画面より新規ユーザー登録

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,7 +4,7 @@ class Admin::UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @users = User.all.order("created_at DESC")
+    @users = User.all.order("created_at DESC").includes(:tasks)
   end
   
   def new
