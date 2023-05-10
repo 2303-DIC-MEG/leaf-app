59ecbdae5bb67804d65591374e0eee511a8e0f91
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-05-10 21:55:05 +0900

    フラッシュコメント変更

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -43,7 +43,7 @@ class Admin::UsersController < ApplicationController
 
   def admin_user
     unless current_user.admin?
-      flash[:danger] = '権限がありません'
+      flash[:notice] = 'NOT Access'
       redirect_to root_url 
     end
   end
