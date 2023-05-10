7fb6c835ff48b62ffcaa064d180960fff027733f
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-05-10 18:19:44 +0900

    adminの設定

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,7 @@
 class Admin::UsersController < ApplicationController
   before_action :admin_user
+  skip_before_action :login_required, only: [:new, :create]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all.order("created_at DESC")
@@ -12,19 +14,42 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      redirect_to user_path(@user.id), notice: "Task was successfully created." 
     else
       render :new
     end
   end
 
-  # def show
-  #   @user = User.find(params[:id])
-  # end
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: "Task was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path, notice: "Task was successfully destroyed"
+  end
 
   private
 
   def admin_user
     redirect_to (root_path) unless current_user.admin?
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :admin, :password_confirmation )
   end
 end
