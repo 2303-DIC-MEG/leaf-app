1658723d2a4e53676a8d2d0146698643d92a1f11
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-05-10 18:39:17 +0900

    管理ユーザーの更新・削除の制御

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,17 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, length: { minimum: 6 }
+
+  before_update :admin_cannot_update
+  before_destroy :admin_cannot_delete
+  
+  private
+  
+  def admin_cannot_update
+    throw :abort if User.exists?(admin: true) && self.saved_change_to_admin == [true, false]
+  end
+  
+  def admin_cannot_delete
+    throw :abort if User.exists?(admin: true) && self.admin == true
+  end
 end
