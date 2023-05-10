c1888d7359be8fdfbf93e9f1fc4b2dd51b66fb19
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-05-10 21:35:30 +0900

    管理画面遷移変更

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -14,7 +14,7 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id), notice: "Task was successfully created." 
+      redirect_to admin_users_path, notice: "Task was successfully created." 
     else
       render :new
     end
