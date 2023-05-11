0be1c498ad293496d669548b05b86fac5b7821c7
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-05-11 20:19:53 +0900

    systemspec実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
-  factory :user do
-    
+  factory :user, class: User do
+    name { "test" }
+    email { "test@gmail.com" }
+    password { "password" }
+    password_confirmation { password }
+    admin {true}
+  end
+  factory :second_user, class: User do
+    name { 'test2' }
+    email { 'test2@gmail.com' }
+    password { "password" }
+    password_confirmation { password }
+    admin {false}
   end
 end
